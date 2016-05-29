8e1348e182065259a0c280fc8157cdc8bd7dd874
zachschleien <zschleien@gmail.com>
2016-05-29 14:53:26 -0400

    Edits to wins actions in controller

--- app/controllers/wins_controller.rb
+++ app/controllers/wins_controller.rb
@@ -3,73 +3,52 @@ class WinsController < ApplicationController
   before_action :correct_user, only: [:edit, :update, :destroy]
   before_action :authenticate_user!, except: [:index, :show]
 
-  # GET /wins
-  # GET /wins.json
   def index
     @wins = Win.all
   end
 
-  # GET /wins/1
-  # GET /wins/1.json
   def show
   end
 
-  # GET /wins/new
   def new
-    @win = Win.new
+    @win = current_user.wins.build # creates new win through current user
   end
 
-  # GET /wins/1/edit
   def edit
   end
 
-  # POST /wins
-  # POST /wins.json
   def create
-    @win = Win.new(win_params)
-
-    respond_to do |format|
+    @win = current_user.wins.build
       if @win.save
-        format.html { redirect_to @win, notice: 'Win was successfully created.' }
-        format.json { render :show, status: :created, location: @win }
+        redirect_to @win, notice: 'Win was successfully created.'
       else
-        format.html { render :new }
-        format.json { render json: @win.errors, status: :unprocessable_entity }
+        render :new
       end
-    end
   end
 
-  # PATCH/PUT /wins/1
-  # PATCH/PUT /wins/1.json
   def update
-    respond_to do |format|
-      if @win.update(win_params)
-        format.html { redirect_to @win, notice: 'Win was successfully updated.' }
-        format.json { render :show, status: :ok, location: @win }
-      else
-        format.html { render :edit }
-        format.json { render json: @win.errors, status: :unprocessable_entity }
-      end
+    if @win.update(win_params)
+      redirect_to @win, notice: 'Win was successfully updated.'
+    else
+      render :edit
     end
   end
 
-  # DELETE /wins/1
-  # DELETE /wins/1.json
   def destroy
     @win.destroy
-    respond_to do |format|
-      format.html { redirect_to wins_url, notice: 'Win was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to wins_url
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_win
       @win = Win.find(params[:id])
     end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
+    def correct_user
+      @win = current_user.wins.find_by(id: params[:id])
+      redirect_to wins_path, notice: "Not authorized to edit this win" if @win.nil?
+    end
+
     def win_params
       params.require(:win).permit(:description)
     end
