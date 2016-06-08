75311fe88a2b07344c4e64eb320f27802c6bfdc4
Skylar Panuska <skylar.panuska@gmail.com>
2016-06-08 17:25:52 -0400

    moves GET /users/:id below devise_for :users so it doesn't override any of those routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # get 'profile/show'
 
   resources :wins
-  get '/users/:id' => 'profile#show'
   
   devise_for :users
   root 'wins#index'
-  get "about" => "pages#about" # about_path
+  get 'about' => 'pages#about' # about_path
+	get '/users/:id' => 'profile#show'
 
 end
